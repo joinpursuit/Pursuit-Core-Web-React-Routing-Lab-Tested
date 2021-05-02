6448bd7aad90c88ba62af670d70b5e75d142d4bd
anjuhypp <anjuhypp@gmail.com>
2021-05-02 17:08:49 -0400

    complete RandomDogByBreed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import { Route, Switch } from "react-router-dom"
-import Links from "./Components/Links"
+import { Route, Switch } from "react-router-dom";
+import Links from "./Components/Links";
 import RandomDogImage from "./Components/RandomDogImage";
 import MultipleRandomDogs from "./Components/MultipleRandomDogs";
-
+import RandomDogByBreed from "./Components/RandomDogByBreed";
 
 function App() {
   return (
@@ -12,11 +12,11 @@ function App() {
         <h1>Hello, world!</h1>
       </main>
       <Switch>
-     <Route path={"/dog/random/:num"} component={MultipleRandomDogs}></Route>
-     <Route path={"/dog/random"} component={RandomDogImage}></Route>
-     {/* <Route path={"/dog/:breed"} component={RandomDogBreed}></Route> */}
-     {/* <Route path={"/cat/random/:num"} component={MultipleRandomCats}></Route> */}
-     {/* <Route path={"/cat/random"} component={RandomCatImage}></Route> */}
+        <Route path={"/dog/random/:num"} component={MultipleRandomDogs}></Route>
+        <Route path={"/dog/random"} component={RandomDogImage}></Route>
+        <Route path={"/dog/:breed"} component={RandomDogByBreed}></Route>
+        {/* <Route path={"/cat/random/:num"} component={MultipleRandomCats}></Route> */}
+        {/* <Route path={"/cat/random"} component={RandomCatImage}></Route> */}
       </Switch>
     </div>
   );
