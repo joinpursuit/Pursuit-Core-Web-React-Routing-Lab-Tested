5b30ca605ec91a853f09fe60b278d09d22815d44
Obaid Rustemi <orustemi@gmail.com>
2021-05-02 16:59:46 -0400

    2nd commit

--- src/Components/RandomDog.js
+++ src/Components/RandomDog.js
@@ -14,6 +14,10 @@ this.setState({url: res.data.message})
         }
     }
 
+    componentDidMount(){
+        this.fetchDog()
+    }
+
     render(){
         const {url} = this.state
         return(
