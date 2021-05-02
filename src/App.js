4e01304cba047cbcfcac67416edc774e5ee9ca9f
Yashiry Azize <azizeyashiry@gmail.com>
2021-05-02 15:26:46 -0400

    set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import RandomDog from "./Components/RandomDog"
+import MultipleDogs from "./Components/MultipleDogs"
+import RandomDogBreed from "./Components/RandomDogBreed"
+import RandomCat from "./Components/RandomCat"
+import MultipleCats from "./Components/MultipleCats"
+import {Route, Switch} from "react-router-dom"
 
 function App() {
   return (
     <div className="app">
-      <main>
-        <h1>Hello, world!</h1>
-      </main>
+      <Switch>
+        
+      </Switch>
     </div>
   );
 }
