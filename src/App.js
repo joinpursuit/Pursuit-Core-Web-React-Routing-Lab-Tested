42e531c1e845c282190b231d531b0a8954f67421
joshuamendez <joshuafmendez@gmail.com>
2021-05-02 17:30:18 -0400

    code clean up and image sizing

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import CatRandomNum from "./Components/CatRandomNum";
 import DogBreed from "./Components/DogBreed";
 import DogRandom from "./Components/DogRandom";
 import DogRandomNum from "./Components/DogRandomNum";
-// import NavBar from "./Components/NavBar";
 import "./App.css";
-
 
 function App() {
   return (
     <div className="app">
-      {/* <DogRandom /> */}
-      {/* <DogRandomNum /> */}
-      {/* <CatRandom /> */}
-      {/* <CatRandomNum /> */}
-      {/* <DogBreed /> */}
-
-      {/* <NavBar /> */}
       <Switch>
         <Route path={"/all/random"} component={Bonus}/>
         <Route path={"/dog/random/:num"} component={DogRandomNum}/>
