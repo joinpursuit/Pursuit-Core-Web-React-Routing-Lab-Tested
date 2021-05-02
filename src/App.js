dcc8c48a21ec34f69631cdc6dc0667373756f8bb
Obaid Rustemi <orustemi@gmail.com>
2021-05-02 15:47:44 -0400

    Random Dog Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import RandomDog from "./Components/RandomDog"
 
 function App() {
   return (
     <div className="app">
-      <main>
-        <h1>Hello, world!</h1>
-      </main>
+      <RandomDog />
     </div>
   );
 }
