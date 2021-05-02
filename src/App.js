cfb3caa2560427a14684d6da35b5390c228accc1
vrwatson <vanessawatson@pursuit.org>
2021-05-02 16:42:09 -0400

    Multiple Random Dogs passing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom"
 import Links from "./Components/Links"
 import RandomDogImage from "./Components/RandomDogImage";
+import MultipleRandomDogs from "./Components/MultipleRandomDogs";
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <h1>Hello, world!</h1>
       </main>
       <Switch>
-     {/* <Route path={"/dog/random/:num"} component={MultipleRandomDogs}></Route> */}
+     <Route path={"/dog/random/:num"} component={MultipleRandomDogs}></Route>
      <Route path={"/dog/random"} component={RandomDogImage}></Route>
      {/* <Route path={"/dog/:breed"} component={RandomDogBreed}></Route> */}
      {/* <Route path={"/cat/random/:num"} component={MultipleRandomCats}></Route> */}
---
+++ src/Components/MultipleRandomDogs.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { Component } from "react";
+
+
+class MultipleRandomDogs extends Component {
+  state = { imageUrls: [] }
+
+  getMultipleDoggies = async () => {
+    try {
+        const {num} = this.props.match.params;
+        const res = await axios.get(`https://dog.ceo/api/breeds/image/random/${num}`)
+        this.setState({ imageUrls: res.data.message })
+    } catch (error) {
+        console.log(error);
+    }
+  }
+    
+  componentDidMount() {
+      this.getMultipleDoggies();
+  }
+  
+  render() {
+      const {imageUrls} = this.state;
+        return(
+            <div>
+                {imageUrls.map(url => {
+                    return <img src={url} alt="Dog pic"/>
+                })}
+            </div>
+        )
+    }
+
+}
+
+export default MultipleRandomDogs;
