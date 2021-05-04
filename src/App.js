0bbbcde62c121628353773cad868d8f04a1bed6e
Kobe <tahiriadam@gmail.com>
2021-05-03 20:53:58 -0400

    completed bonus

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Dogs from './components/Dogs'
 import DogBreed from './components/DogBreed'
 import RandomCat from './components/RandomCat'
 import Cats from './components/Cats'
+import CatsAndDogs from './components/CatsAndDogs'
 import { Route, Switch } from 'react-router-dom'
 import "./App.css";
 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
         <Route path={"/dog/random/:num"} component={Dogs} />
         <Route path={"/cat/random/:num"} component={Cats} />
+        <Route path={"/all/random"} component={CatsAndDogs} />
         <Route exact path={"/dog/random"} component={RandomDog} />
         <Route exact path={"/cat/random"} component={RandomCat} />
         <Route path={"/dog/:breed"} component={DogBreed} />
