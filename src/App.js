27df1665972ca3d2891a0c0996fc7350672388c0
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-05-03 22:35:16 -0400

    cat routes completed

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
     <div className="app">
       Hello, world!
       <Switch>
+        <Route path={"/dog/random/:num"} component={MultipleRandomDogs} />
         <Route exact path={"/dog/random"} component={RandomDogImage} />
-        <Route exact path={"/dog/random/:num"} component={MultipleRandomDogs} />
         <Route path={"/dog/:breed"} component={RandomDogByBreed} />
-        <Route path={"/cat/random"} component={RandomCatImage} />
         <Route path={"/cat/random/:num"} component={MultipleRandomCats} />
+        <Route path={"/cat/random"} component={RandomCatImage} />
         {/* <Route exact path={"/"} component={Home} />
         <Route path={"/*"} component={FourOFour} /> */}
       </Switch>
--- src/Components/RandomCatImage.js
+++ src/Components/RandomCatImage.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import axios from "axios";
 
 class RandomCatImage extends Component {
+    state = { catURL: "" }
+
+    componentDidMount() {
+        this.fetchCat();
+    }
+    
+    fetchCat = async() => {
+        try {
+            const res = await axios.get(`https://api.thecatapi.com/v1/images/search`);
+            this.setState({catURL: res.data[0].url})
+        } catch (error) {
+            console.log(error);
+            this.setState({catURL: ""})
+        }
+    }
+
     render() {
+        const { catURL } = this.state;
+
         return (
             <div>
                 RandomCatImage
+                <button value={catURL} onClick={this.fetchCat}>Get New Cat</button>
+                <img src={catURL} alt="random cat"/>
             </div>
         )
     }
