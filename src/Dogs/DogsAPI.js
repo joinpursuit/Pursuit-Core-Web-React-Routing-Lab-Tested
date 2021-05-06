415db4a23caf125e8ea71fb65a94e11cd77a59b0
cassidybeni <cassidybeni@pursuit.org>
2021-05-06 10:04:58 -0400

    Incomplete

--- src/Dogs/DogsAPI.js
+++ src/Dogs/DogsAPI.js
@@ -1,12 +1,18 @@
 import axios from "axios"
 
-const getDog =  async() => {
+const getDog =  async () => {
     const { data } = await axios.get(`https://dog.ceo/api/breeds/image/random`)
     return data.message
 }
 
+const getMultipleDogs = async (num) => {
+    const { data } = await axios.get(`https://dog.ceo/api/breeds/image/random/${num}`)
+    return data.map((dogObj) => dogObj.message)
+}
+
 const dogsAPI = {
     getDog,
+    getMultipleDogs,
 }
 
 export default dogsAPI
--- src/Dogs/index.js
+++ src/Dogs/index.js
@@ -3,11 +3,15 @@ import { Switch, Route} from "react-router-dom"
 import DogRandom from "./DogRandom"
 import DogRandomNum from "./DogRandomNum"
 
+const renderDogRandomNum = ({ match }) => {
+    return <DogRandomNum num={ match.params.num } />
+}
+
 function Dogs() {
     return (
         <div>
             <Switch>
-                <Route path="/dog/random/:num" />
+                <Route path="/dog/random/:num" render={renderDogRandomNum} />
                 <Route path="/dog/random" component={DogRandom} />
             </Switch>
         </div>
